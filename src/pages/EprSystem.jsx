e1e5c73367be65cb9e497371034a856df5c0f8a3
kaleab gemechu <kgemechu908@gmail.com>
2024-08-26 12:13:52 +0300

    added onscroll animation to the EPR page

--- src/pages/EprSystem.jsx
+++ src/pages/EprSystem.jsx
@@ -50,7 +50,13 @@ function EprSystem() {
         </div>
       </div> */}
       <div className="max-w-[1000px] m-auto grid phone:grid-cols-1 mtab:grid-cols-2 items-center gap-4 py-20">
-        <div className="justify-self-center flex flex-col gap-6">
+        <motion.div
+          variants={fadeIn("right", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="justify-self-center flex flex-col gap-6"
+        >
           <div>
             <img src="star.png" className="block h-[50px] w-[50px]" />
           </div>
@@ -75,19 +81,37 @@ function EprSystem() {
               </li>
             </ul>
           </div>
-        </div>
-        <div className="flex justify-center">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="flex justify-center"
+        >
           <img
             src="subscribe.png"
             className="block w-full h-full phone:w-3/4 phone:w-3/4"
           />
-        </div>
+        </motion.div>
       </div>
       <div className="max-w-[1000px] m-auto grid phone:grid-cols-1 mtab:grid-cols-2 items-center gap-6 my-10">
-        <div className="flex justify-center">
+        <motion.div
+          variants={fadeIn("right", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="flex justify-center"
+        >
           <img src="service.png" className="block phone:w-3/4 phone:h-3/4" />
-        </div>
-        <div className="w-3/4 justify-self-center flex flex-col gap-4 my-6">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="w-3/4 justify-self-center flex flex-col gap-4 my-6"
+        >
           <div className="ml-4">
             <img src="star.png" className="block h-[35px] w-[35px]" />
           </div>
@@ -132,10 +156,16 @@ function EprSystem() {
               </li>
             </ul>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="max-w-[1000px] m-auto grid phone:grid-cols-1 mtab:grid-cols-2 items-center gap-4">
-        <div className="justify-self-center flex flex-col gap-6 mt-4">
+        <motion.div
+          variants={fadeIn("right", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="justify-self-center flex flex-col gap-6 mt-4"
+        >
           <div className="ml-4">
             <img src="star.png" className="block h-[35px] w-[35px]" />
           </div>
@@ -172,16 +202,34 @@ function EprSystem() {
               </li>
             </ul>
           </div>
-        </div>
-        <div className="flex justify-center my-6">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="flex justify-center my-6"
+        >
           <img src="system.png" className="block phone:w-3/4 phone:h-3/4" />
-        </div>
+        </motion.div>
       </div>
       <div className="max-w-[1000px] m-auto grid phone:grid-cols-1 mtab:grid-cols-2 items-center gap-4 my-10">
-        <div className="items-center flex flex-col gap-6 mt-16">
+        <motion.div
+          variants={fadeIn("right", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="items-center flex flex-col gap-6 mt-16"
+        >
           <img src="free.png" className="block phone:w-3/4 phone:w-3/4" />
-        </div>
-        <div className="justify-self-center flex flex-col gap-6">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="justify-self-center flex flex-col gap-6"
+        >
           <div className="">
             <ServiceCard
               image="software.png"
@@ -197,7 +245,7 @@ function EprSystem() {
           >
             learn more
           </Button>
-        </div>
+        </motion.div>
       </div>
       <Footer />
     </div>
