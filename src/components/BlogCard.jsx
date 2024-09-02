dab7883924f42c807b008b0190639457056ec002
kaleab gemechu <kgemechu908@gmail.com>
2024-09-02 17:30:39 +0300

    updated the blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -14,33 +14,79 @@ const BlogCard = () => {
   console.log(content);
 
   return (
-    <div className="grid phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-y-10 px-4">
+    <div className="grid phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-y-[50px] px-4">
       {content.map((item, i) => (
-        <div
-          key="i"
-          className=" group z-10 max-w-sm m-auto rounded-t-lg rounded-b-lg overflow-hidden transition-all px-4"
-        >
-          <div className="rounded-lg rounded-b-lg overflow-hidden h-3/5">
-            <img
-              src={item.img}
-              className="w-full h-full scale-100 hover:cursor-pointer hover:scale-105"
-            />
-          </div>
-          <div className="flex items-center justify-center gap-2 border-b-[1px]  mx-6">
-            {item.calanderIcon}
-            <p className="text-secondary text-center p-4 transition-apacity delay-100 hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
-              {item.date}
-            </p>
-            {item.adminIcon}
-            <p className="hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
-              {item.name}
-            </p>
-          </div>
+        <>
+          <div
+            key="i"
+            className=" group z-10 max-w-sm m-auto rounded-t-lg rounded-b-lg overflow-hidden transition-all px-4"
+          >
+            <div className="rounded-lg rounded-b-lg overflow-hidden h-3/5">
+              <img
+                src={item.img}
+                className="w-full h-[15rem] scale-100 hover:cursor-pointer hover:scale-105"
+              />
+            </div>
+            <div className="flex items-center justify-center gap-2 border-b-[1px]  mx-6">
+              {item.calanderIcon}
+              <p className="text-secondary text-center p-4 transition-apacity delay-100 hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
+                {item.date}
+              </p>
+              {item.adminIcon}
+              <p className="hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
+                {item.name}
+              </p>
+            </div>
 
-          <h2 className="px-[2px] py-4 capitalize text-primary font-colasta text-primary text-[17px] text-left align-middle break-normal font-semibold hover:cursor-pointer hover:text-btn-main hover:text-btn-bg-main transition-all duration-[300ms]">
-            {item.title}
-          </h2>
-        </div>
+            <h2 className="px-[2px] py-4 capitalize text-primary font-colasta text-primary text-[17px] text-left align-middle break-normal font-semibold hover:cursor-pointer hover:text-btn-main hover:text-btn-bg-main transition-all duration-[300ms]">
+              {item.title}
+            </h2>
+            <div className="mb-[50px]">
+              <Button
+                variant="primary"
+                size="sm"
+                className="bg-btn-bg-main font-bold"
+              >
+                explore more
+              </Button>
+            </div>
+
+            <div
+              key="i"
+              className=" group z-10 max-w-sm m-auto rounded-t-lg rounded-b-lg overflow-hidden transition-all"
+            >
+              <div className="rounded-lg rounded-b-lg overflow-hidden h-3/5">
+                <img
+                  src={item.img}
+                  className="w-full h-[15rem] scale-100 hover:cursor-pointer hover:scale-105"
+                />
+              </div>
+              <div className="flex items-center justify-center gap-2 border-b-[1px]  mx-6">
+                {item.calanderIcon}
+                <p className="text-secondary text-center p-4 transition-apacity delay-100 hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
+                  {item.date}
+                </p>
+                {item.adminIcon}
+                <p className="hover:cursor-pointer hover:text-btn-bg-main transition-all duration-[300ms]">
+                  {item.name}
+                </p>
+              </div>
+
+              <h2 className="px-[2px] py-4 capitalize text-primary font-colasta text-primary text-[17px] text-left align-middle break-normal font-semibold hover:cursor-pointer hover:text-btn-main hover:text-btn-bg-main transition-all duration-[300ms]">
+                {item.title}
+              </h2>
+              <div className="">
+                <Button
+                  variant="primary"
+                  size="sm"
+                  className="bg-btn-bg-main font-bold"
+                >
+                  explore more
+                </Button>
+              </div>
+            </div>
+          </div>
+        </>
       ))}
     </div>
   );
